3f19fc2f62f421d0efdd83c389beb0cb5460b25a
Szegedi Tamás <102833465+Shinjiterubx@users.noreply.github.com>
2023-05-08 12:05:34 +0200

    fix: navbar
    Megcsináltam a navbarban az eddigi gombokat

--- afp2-neptun/resources/views/layouts/_header.blade.php
+++ afp2-neptun/resources/views/layouts/_header.blade.php
@@ -2,10 +2,13 @@
     <div class="container-fluid"><img src="{{URL::asset('/imgs/logo.png')}}" width="58" height="38"><a class="navbar-brand" href="#" style="font-family: 'JetBrains Mono', monospace;color: rgba(255,255,255,0.9);">Neptun+</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse d-xl-flex justify-content-end justify-content-xl-end" id="navcol-1">
             <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="#" style="font-family: 'JetBrains Mono', monospace;color: rgb(255,255,255);">Jegyek</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url("student/marks") }}" style="font-family: 'JetBrains Mono', monospace;color: rgb(255,255,255);">Jegyek</a></li>
                 <li class="nav-item"><a class="nav-link" href="#" style="font-family: 'JetBrains Mono', monospace;color: rgb(255,255,255);">Hiányzások</a></li>
-                <li class="nav-item"><a class="nav-link" href="#" style="font-family: 'JetBrains Mono', monospace;color: rgb(255,255,255);">Tantárgyak</a></li>
-                <li class="nav-item"><a class="nav-link" href="#" style="font-family: 'JetBrains Mono', monospace;">Kijelentkezés</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url("subjects/mysubjects") }}" style="font-family: 'JetBrains Mono', monospace;color: rgb(255,255,255);">Tantárgyak</a></li>
+                <li class="nav-item"><form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button class="nav-link" style="font-family: 'JetBrains Mono', monospace;">Kijelentkezés</button></li>
+                </form>
             </ul>
         </div>
     </div>
